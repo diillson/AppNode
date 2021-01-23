8cbec8e16c86fe1b52fd319f0f0cca06480a2a52
Edilson Freitas <diillson@hotmail.com>
2021-01-23 18:44:37 -0300

    MOD ADD

--- app/server.js
+++ app/server.js
@@ -19,24 +19,33 @@ var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 // get all notes
-server.get('/notes', function (request, response, next) {
+server.get('/notas', function (request, response, next) {
 	connection.query('select * from notes.Note order by Id desc', function (error, results, fields) {
 		if (error) { next(error); return; }
 		response.end(JSON.stringify(results));
 	});
 });
 
+server.get('/', function tempo(request, response, next) {
+			const dynamicDate = new Date();
+
+			response.json({date: dynamicDate.toGMTString(),
+			Projeto: "API de integração com Banco MySql"
+			});
+				
+});
+
 // create note
-server.post('/notes', function (request, response, next) {
+server.post('/notas', function (request, response, next) {
 	if(!request.body) { return next(new errors.BadRequestError("texto inválido")); }
 	connection.query('insert into notes.Note (Text) values ("?")', [request.body], function (error, results, fields) {
 		if (error) { next(error); return; }
-		response.end("Ok");
+		response.end("Computado");
 	});
 });
 
 // delete note
-server.del('/notes/:id', function (request, response, next) {
+server.del('/notas/:id', function (request, response, next) {
 	var id = request.params.id;
 	
 	if(!id || id <= 0) { return next(new errors.BadRequestError("id inválido")); }
@@ -44,7 +53,7 @@ server.del('/notes/:id', function (request, response, next) {
 	connection.query('delete from notes.Note WHERE Id=?', [id], function (error, results, fields) {
 		if (error) { next(error); return; }
 		if(!results.affectedRows) { next(new errors.BadRequestError("id inválido")); return; }
-		response.end("Ok");
+		response.end("Deletado");
 	});
 });
 
